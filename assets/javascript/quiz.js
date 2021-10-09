6792040ce20c558c5921dd27eefbdbb5fea6116f
Jason Offenbaker <JasonOffenbaker@gmail.com>
2021-10-09 00:01:28 -0700

    Added quiz loop for retrying and add a notification for if an answer is right or wrong

--- assets/javascript/quiz.js
+++ assets/javascript/quiz.js
@@ -3,22 +3,39 @@ var startBtn = document.getElementById('start');
 var subBtn = document.getElementById('submit')
 var timerEl = document.getElementById('countdown');
 var questionEl = document.getElementById('currentQuestion');
+var userInputEl = document.getElementById('formHidden');
+var submitButtonEl = document.getElementById('buttonSpan');
+var isCorrectEl = document.getElementById('isCorrect');
+
+
 var message =
-    'Question 1,Question 2,Question 3,Question 4,Question 5,Question 6,';
+    'Question 2,Question 3,Question 4,Question 5,';
 var words = message.split(',');
+questionAnswers = [1, 2, 3, 4, 5];
 var wordCount = 0;
+var userInput = document.getElementById("submit").submit();
+var isRetry = false;
+var isCorrect = null;
+var timeLeft = 100;
+var questionsFalse = 0;
 
 
 
 //Countdown function
 function countdown() {
     //Variable Declarations
-    var timeLeft = 100;
+    timeLeft = 100;
     //Set initial question.
-    questionEl.textContent = "This is a question?"
+    questionEl.textContent = "Question 1"
     //make submit button visible
-    document.getElementById("Button-hidden").id = "Button-visible"
-    document.getElementById("start").id = "Button-hidden"
+    if (isRetry == false) {
+        document.getElementById("Button-hidden").id = "Button-visible"
+        document.getElementById("formHidden").id = "Button-visible"
+        document.getElementById("start").id = "Button-hidden"
+    } else {
+        submitButtonEl.textContent = "Next Question"
+        questionsFalse = 0;
+    }
     //SetInterval function for timer
     var timeInterval = setInterval(function () {
         if (timeLeft > 0) {
@@ -29,6 +46,7 @@ function countdown() {
             //Stop the timer once it hits 0
             clearInterval(timeInterval);
         }
+
     }, 1000);
     subBtn.onclick = nextQuestion;
 
@@ -36,12 +54,33 @@ function countdown() {
     function nextQuestion() {
         questionEl.textContent = words[wordCount];
         if (wordCount > words[wordCount]) {
-            questionEl.textContent = "You answered all the questions with a score of " + timeLeft + "!";
-            document.getElementById('Button-visible').id = 'Button-hidden'
+            questionEl.textContent = "You answered all the questions with a score of " + timeLeft + "! with " + questionsFalse + " questions wrong";
             clearInterval(timeInterval);
+            wordCount++;
+            if (wordCount == 5) {
+                submitButtonEl.textContent = "Enter you initials and Try again"
+            }
+        } else {
+            wordCount++;
         }
         console.log('wordCount is equal to ' + wordCount);
-        wordCount++;
+        if (wordCount == 6) {
+            wordCount = 0;
+            isRetry = true;
+            countdown();
+        }
+
+        if (questionAnswers == 1) {
+            isCorrect = true;
+            isCorrectEl.textContent = "Correct"
+        } else {
+            isCorrect = false;
+            isCorrectEl.textContent = "Incorrect"
+            timeLeft = timeLeft - 15;
+            questionsFalse++;
+
+        }
+        console.log(userInput);
     }
 
 }
