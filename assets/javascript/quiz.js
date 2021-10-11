d3208b003aa4bcfcc5e7cd96cd78669746953e36
Jason Offenbaker <JasonOffenbaker@gmail.com>
2021-10-10 21:20:16 -0700

    added answers to the question s

--- assets/javascript/quiz.js
+++ assets/javascript/quiz.js
@@ -8,16 +8,15 @@ var submitButtonEl = document.getElementById('buttonSpan');
 var isCorrectEl = document.getElementById('isCorrect');
 
 
-var message =
-    'Question 2,Question 3,Question 4,Question 5,';
-var words = message.split(',');
-questionAnswers = [1, 2, 3, 4, 5];
+var questions = ["What does HTML stand for?", "What does CSS Stand for?", "What is a .js file?", "what is the <body> of a page?", "when calling for CSS styles you call them by their?", "Im getting an error when using this code 'getElementById' what should it be instead?"]
+questionAnswers = ["hyper text markup language", "cascading style sheets", "javascript", "Where the content goes", "selectors", "document.getelementbyid"];
 var wordCount = 0;
-var userInput = document.getElementById("submit").submit();
+var userInput = document.getElementById("submit");
 var isRetry = false;
 var isCorrect = null;
 var timeLeft = 100;
 var questionsFalse = 0;
+var initials = "";
 
 
 
@@ -26,11 +25,11 @@ function countdown() {
     //Variable Declarations
     timeLeft = 100;
     //Set initial question.
-    questionEl.textContent = "Question 1"
+    questionEl.textContent = questions[0];
     //make submit button visible
     if (isRetry == false) {
         document.getElementById("Button-hidden").id = "Button-visible"
-        document.getElementById("formHidden").id = "Button-visible"
+        document.getElementById("formHidden").id = "formVisible"
         document.getElementById("start").id = "Button-hidden"
     } else {
         submitButtonEl.textContent = "Next Question"
@@ -41,10 +40,14 @@ function countdown() {
         if (timeLeft > 0) {
             timeLeft--;
             timerEl.textContent = timeLeft;
-            console.log(timeLeft);
+            //console.log(timeLeft);
         } else {
             //Stop the timer once it hits 0
             clearInterval(timeInterval);
+        }
+        if (timeLeft < 0) {
+            wordCount = 5;
+            nextQuestion();
         }
 
     }, 1000);
@@ -52,35 +55,42 @@ function countdown() {
 
     //Cycles through the question
     function nextQuestion() {
-        questionEl.textContent = words[wordCount];
-        if (wordCount > words[wordCount]) {
-            questionEl.textContent = "You answered all the questions with a score of " + timeLeft + "! with " + questionsFalse + " questions wrong";
-            clearInterval(timeInterval);
-            wordCount++;
-            if (wordCount == 5) {
-                submitButtonEl.textContent = "Enter you initials and Try again"
-            }
+        wordCount++
+        userInput = document.getElementById("formVisible").value;
+        //console.log(questionAnswers[0])
+        //console.log(userInput)
+        if (userInput == questionAnswers[wordCount - 1]) {
+            isCorrect = true;
+            isCorrectEl.textContent = "Correct!"
         } else {
-            wordCount++;
+            timeLeft = timeLeft - 15;
+            timerEl.textContent = timeLeft;
+            isCorrect = false;
+            isCorrectEl.textContent = "Incorrect"
+            questionsFalse++;
         }
-        console.log('wordCount is equal to ' + wordCount);
-        if (wordCount == 6) {
+        questionEl.textContent = questions[wordCount];
+        if (wordCount > 5) {
+            if (timeLeft < 0) {
+                questionEl.textContent = "You answered all the questions with a score of 0! with " + questionsFalse + " questions wrong";
+                timeLeft = 15;
+            } else {
+                questionEl.textContent = "You answered all the questions with a score of " + timeLeft + "! with " + questionsFalse + " questions wrong";
+            }
+            submitButtonEl.textContent = "Enter you initials and Try again"
+            initials = document.getElementById("formVisible").value;
+            localStorage.setItem("Initials", initials)
+            localStorage.setItem("Score", timeLeft)
+            clearInterval(timeInterval);
+        }
+        //console.log('wordCount is equal to ' + wordCount);
+        if (wordCount == 7) {
             wordCount = 0;
             isRetry = true;
             countdown();
         }
-
-        if (questionAnswers == 1) {
-            isCorrect = true;
-            isCorrectEl.textContent = "Correct"
-        } else {
-            isCorrect = false;
-            isCorrectEl.textContent = "Incorrect"
-            timeLeft = timeLeft - 15;
-            questionsFalse++;
-
-        }
-        console.log(userInput);
+        //console.log(userInput);
+        userInputEl.value = "";
     }
 
 }
